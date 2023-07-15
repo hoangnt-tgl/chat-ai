daf87dc77c8a9dc1c1902f31e643f1f93edfd9a2
Cena448 <space.phamhuunghia1212@gmail.com>
2023-07-15 09:17:36 +0700

    Set key to env file

--- server.js
+++ server.js
@@ -2,13 +2,15 @@ import express from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
 import { Configuration, OpenAIApi } from 'openai';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
-const key1 = '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';
-const key2 = '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';
-const key3 = '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';
+const key1 = process.env.KEY_1;
+const key2 = process.env.KEY_2;
+const key3 = process.env.KEY_3;
 let arrayKey = [key1, key2, key3];
 
 app.post('/api/chat',async (req, res) => {
